d31ec86f6996bb8504e744da20a62d3ef7a5a626
RunningLearner <tkacnsdms@naver.com>
2022-12-20 20:40:10 +0900

    feat:post 기능 추가

--- react-prac/src/App.js
+++ react-prac/src/App.js
@@ -1,18 +1,42 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const App = () => {
-  const [number, setNumber] = useState(1);
-
-  const double = () => {
-    setNumber(number * 2);
-    console.log(number);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const onSubmit = () => {
+    axios.post("http://localhost:3001/posts", {
+      title: title,
+      body: body,
+    });
   };
 
   return (
-    <>
-      <div>{number}</div>
-      <button onClick={double}>submit</button>
-    </>
+    <div className="container ">
+      <div className="mb-3">
+        <label className="form-label">Title</label>
+        <input
+          className=" form-control"
+          value={title}
+          onChange={(event) => {
+            setTitle(event.target.value);
+          }}
+        />
+      </div>
+      <div className="mb-3">
+        <label className="form-label">Body</label>
+        <input
+          className=" form-control"
+          value={body}
+          onChange={(event) => {
+            setBody(event.target.value);
+          }}
+        />
+      </div>
+      <button className="btn btn-primary" onClick={onSubmit}>
+        Post
+      </button>
+    </div>
   );
 };
 
