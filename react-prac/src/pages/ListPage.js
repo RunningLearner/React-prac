acf040f569fe8d46fbfa370aae5fe037b5c354b6
RunningLearner <tkacnsdms@naver.com>
2022-12-24 15:20:52 +0900

    feat: state 와 effect로 데이터 상태관리

--- react-prac/src/pages/ListPage.js
+++ react-prac/src/pages/ListPage.js
@@ -1,6 +1,18 @@
-import React from "react";
+import {useState, useEffect},React from "react";
+import axios from "axios";
 
-const ListPage = () => {
+const ListPage = async () => {
+const [posts, setPosts] = useState([])
+
+    const getPosts = () => {
+    const res = await axios.get("http://localhost:3001/posts");
+    setPosts(res.data)
+  };
+
+  useEffect(() => {
+    getPosts()
+  }, [])
+  
   return <div>L</div>;
 };
 
