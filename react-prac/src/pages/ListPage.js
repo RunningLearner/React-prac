ffa0629ceabb2656d5460c69f961c9c0f886e927
RunningLearner <tkacnsdms@naver.com>
2022-12-26 10:47:59 +0900

    feat: card 컴포넌트 추가

--- react-prac/src/pages/ListPage.js
+++ react-prac/src/pages/ListPage.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import Card from "../components/Card";
 
 const ListPage = () => {
   const [posts, setPosts] = useState([]);
@@ -13,7 +14,14 @@ const ListPage = () => {
     getPosts();
   }, []);
 
-  return <div>L</div>;
+  return (
+    <div>
+      <h1>Blogs</h1>
+      {posts.map((post) => {
+        return <Card key={post.id} title={post.title} />;
+      })}
+    </div>
+  );
 };
 
 export default ListPage;
