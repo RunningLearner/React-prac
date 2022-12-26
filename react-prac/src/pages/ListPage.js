c6fcb04584c0f23b233c60a02d6df2d8876af2ac
RunningLearner <tkacnsdms@naver.com>
2022-12-26 14:22:14 +0900

    feat: 로딩 스피너 컴포넌트 추가

--- react-prac/src/pages/ListPage.js
+++ react-prac/src/pages/ListPage.js
@@ -3,14 +3,17 @@ import axios from "axios";
 import Card from "../components/Card";
 import { Link } from "react-router-dom";
 import { useHistory } from "react-router";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 const ListPage = () => {
   const history = useHistory();
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const getPosts = async () => {
     const res = await axios.get("http://localhost:3001/posts");
     setPosts(res.data);
+    setLoading(false);
   };
 
   const deleteBlog = async (e, id) => {
@@ -29,6 +32,37 @@ const ListPage = () => {
     getPosts();
   }, []);
 
+  const renderBlogList = () => {
+    if (loading) {
+      return <LoadingSpinner />;
+    }
+
+    if (posts.length === 0) {
+      return <div>"No blog posts found!"</div>;
+    }
+
+    return posts.map((post) => {
+      return (
+        <Card
+          key={post.id}
+          title={post.title}
+          onClick={() => {
+            history.push("/blogs/edit");
+          }}
+        >
+          <div>
+            <button
+              className="btn btn-danger"
+              onClick={(e) => deleteBlog(e, post.id)}
+            >
+              Delete
+            </button>
+          </div>
+        </Card>
+      );
+    });
+  };
+
   return (
     <div>
       <div className="d-flex justify-content-between">
@@ -39,26 +73,7 @@ const ListPage = () => {
           </Link>
         </div>
       </div>
-      {posts.map((post) => {
-        return (
-          <Card
-            key={post.id}
-            title={post.title}
-            onClick={() => {
-              history.push("/blogs/edit");
-            }}
-          >
-            <div>
-              <button
-                className="btn btn-danger"
-                onClick={(e) => deleteBlog(e, post.id)}
-              >
-                Delete
-              </button>
-            </div>
-          </Card>
-        );
-      })}
+      {renderBlogList()}
     </div>
   );
 };
