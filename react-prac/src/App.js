d34fcae4e79568c1f349088199fa46c9a7d63471
RunningLearner <tkacnsdms@naver.com>
2022-12-26 10:47:20 +0900

    fix: navbar 밑에 마진

--- react-prac/src/App.js
+++ react-prac/src/App.js
@@ -8,7 +8,7 @@ const App = () => {
     <Router>
       <NavBar />
 
-      <div className="container ">
+      <div className="container mt-3">
         <Switch>
           {routes.map((route) => {
             return (
