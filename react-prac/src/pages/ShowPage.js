102f7abea979775235c7aba3cde3d41e9259c546
RunningLearner <tkacnsdms@naver.com>
2022-12-27 10:54:44 +0900

    feat:상세페이지 기능 구현

--- react-prac/src/pages/ShowPage.js
+++ react-prac/src/pages/ShowPage.js
@@ -1,10 +1,34 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 const ShowPage = () => {
   const { id } = useParams();
+  const [post, setPost] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  return <div></div>;
+  const getPost = (id) => {
+    axios.get(`http://localhost:3001/posts/${id}`).then((res) => {
+      setPost(res.data);
+      setLoading(false);
+    });
+  };
+
+  useEffect(() => {
+    getPost(id);
+  }, []);
+
+  if (loading) {
+    return <LoadingSpinner />;
+  }
+
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  );
 };
 
 export default ShowPage;
